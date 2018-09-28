550e7eb38d5c19bb7daeb6e7847565a1cb4662fc
Andrew Townsend <AndrewTownsend@EducationCity.com>
2018-09-28 15:37:35 +0100

    Continuing to fill out the MQTT/WiFi Connection processes to maintain the MQTT connection over the lifetime of the application.

--- src/main.cpp
+++ src/main.cpp
@@ -29,19 +29,85 @@
 #define MQTT_SERVER_PORT       1883
 #define MQTT_APP               "kitchen/cabinet-lights"
 #define MQTT_COMMAND_TOPIC     "/cmd"
-#define MQTT_STATE_TOPIC       "/state"
+#define MQTT_STATE_TOPIC       "-controller/state"
 
-/*-----( Initialise objects )-----*/
+/*-----( Initialise Global objects )-----*/
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
 
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER_ADDR, MQTT_SERVER_PORT);
 
+/*-----( Initialise MQTT topics )-----*/
+// Setup a feed called 'stateTopic' for publishing.
+// Feed: kitchen/cabinet-lights-controller/state (ONLINE|OFFLINE)
+Adafruit_MQTT_Publish stateTopic = Adafruit_MQTT_Publish(&mqtt,  MQTT_APP MQTT_STATE_TOPIC);
+
+// Setup a feed called 'commandTopic' for publishing.
+// Feed: kitchen/cabinet-lights-controller/state ($HEX)
+Adafruit_MQTT_Publish commandTopic = Adafruit_MQTT_Publish(&mqtt,  MQTT_APP MQTT_STATE_TOPIC);
+
+/*-----( Sketch Functions )-----*/
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void connectMQTT() {
+    int8_t ret;
+
+    // Stop if already connected.
+    if (mqtt.connected()) {
+        return;
+    }
+
+    Serial.print("Connecting to MQTT... ");
+
+    uint8_t retries = 3;
+    while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+        Serial.println(mqtt.connectErrorString(ret));
+        Serial.println("Retrying MQTT connection in 10 seconds...");
+        mqtt.disconnect();
+        delay(10000);  // wait 10 seconds
+        retries--;
+        if (retries == 0) {
+            // basically die and wait for WDT to reset me
+            while (1);
+        }
+    }
+    Serial.println("MQTT Connected!");
+}
+
+void connectWiFi() {
+    // Connect to the WiFi access point.
+    Serial.println(); 
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(WLAN_SSID);
+
+    WiFi.begin(WLAN_SSID, WLAN_PASS);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+
+    Serial.println("WiFi connected");   
+}
+
 void setup() {
     // put your setup code here, to run once:
+    Serial.begin(115200);
+    delay(10);
+
+    Serial.println(F("NodeMCU MQTT IR Receiver"));
+
+    // Connect to the WiFi
+    connectWiFi();
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
+
+    // Ensure the connection to the MQTT server is alive (this will make the first
+    // connection and automatically reconnect when disconnected).  See the MQTT_connect
+    // function definition further below.
+    connectMQTT();
 }
