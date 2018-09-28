31fda2fde2143936662c8c0b71978c96e2328c88
Andrew Townsend <AndrewTownsend@EducationCity.com>
2018-09-28 15:20:22 +0100

    Adding the boilerplate for the project and the dependant libraries

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,27 @@
 
 /*-----( Include Require Libraries )-----*/
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+/*-----( Declare WiFi objects )-----*/
+#define WLAN_SSID       "TP-Link_IOT"
+#define WLAN_PASS       "iLikeTurtles"
+
+/*-----( Declare MQTT objects )-----*/
+#define MQTT_SERVER_ADDR       "10.71.7.92"
+#define MQTT_SERVER_PORT       1883
+#define MQTT_APP               "kitchen/cabinet-lights"
+#define MQTT_COMMAND_TOPIC     "/cmd"
+#define MQTT_STATE_TOPIC       "/state"
+
+/*-----( Initialise objects )-----*/
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER_ADDR, MQTT_SERVER_PORT);
 
 void setup() {
     // put your setup code here, to run once:
