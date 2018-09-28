2743953ba0cc456733d336aeccef4bc1c85ce4b0
Andrew Townsend <AndrewTownsend@EducationCity.com>
2018-09-28 14:58:58 +0100

    Updating the file docblock, to include the correct variables and description.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+/**
+ * Arduino Infrared Decoder
+ * 
+ * @architecture NodeMCU ESP8266 ESP-12E
+ * @framework Arduino
+ * 
+ * @version 2.0
+ * @date 18/03/2018
+ * 
+ * @author Andrew Towensend <andrew@exclusivewebsystems.com>
+ * @description Decodes data from the Kitchen Lighting IR Remote Control into its
+ *              respective hex commands, this is published into a MQTT Topic via 
+ *              the built in WiFi.
+ * 
+ */
+
+/*-----( Include Require Libraries )-----*/
 #include <Arduino.h>
 
 void setup() {
